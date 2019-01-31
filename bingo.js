0f0d81a10544bde868cb16a48428a28b9b6cb4fb
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-02-01 01:07:14 +0900

    UPDATE: Check Bingo

--- bingo.js
+++ bingo.js
@@ -1,14 +1,25 @@
 new Vue({
   el: '#app',
   created: function () {
-    
+      // check parameters
   },
   data: function () {
     return {
       name: '',
       selected: [],
-      data: data
+      data: data,
+      bingo: 0
     }
   },
-  methods: {}
+  methods: {
+    click: function (idx) {
+      console.log(idx)
+      var square = document.getElementById(`square-${idx}`)
+      if (square.classList.contains('selected')) {
+        square.classList.remove('selected');
+      } else {
+        square.classList.add('selected');
+      }
+    }
+  }
 });
