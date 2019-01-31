5c92e5ff9880a96219ba71f9274c7890ec794851
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-02-01 08:34:48 +0900

    ADD: Pack & Load bingo data

--- bingo.js
+++ bingo.js
@@ -1,7 +1,7 @@
 new Vue({
   el: '#app',
   created: function () {
-      // check parameters
+    // check parameters
   },
   data: function () {
     return {
@@ -16,10 +16,26 @@ new Vue({
       console.log(idx)
       var square = document.getElementById(`square-${idx}`)
       if (square.classList.contains('selected')) {
-        square.classList.remove('selected');
+        square.classList.remove('selected')
+        this.selected.splice(this.selected.indexOf(idx), 1)
       } else {
-        square.classList.add('selected');
+        square.classList.add('selected')
+        this.selected.push(idx)
+        this.selected.sort((a, b) => a - b)
       }
+      console.log(this.selected)
+    },
+    pack: function (array) {
+      var packed = []
+      for (let i = 0; i < array.length; i++) {
+        let square = array[i].toString(16)
+        if (square.length % 2) { square = '0' + square }
+        packed.push(square)
+      }
+      return packed.join('')
+    },
+    load: function (packed) {
+      return packed.match(/\d\d/gi).map(t => parseInt(t))
     }
   }
-});
+})
