03b3809d13b90789c3928dc7fa81efd25c6afe7f
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-02-01 23:25:10 +0900

    UPDATE: Finish parsing from packed

--- bingo.js
+++ bingo.js
@@ -2,9 +2,29 @@ new Vue({
   el: '#app',
   created: function () {
     // check parameters
+    var get = [] 
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(a,name,value){get[name]=value;})
+    var t = get['t']
+    var f = get['f']
+    var name = get['name']
+    if (name !== undefined && (t !== undefined || f !== undefined)) {
+      this.view = true
+      this.name = decodeURIComponent(name)
+      if (t) {
+        this.selected = this.unpack(t)
+      } else {
+        unselected = this.unpack(f)
+        for (let i = 0; i < 25; i++) {
+          if (!unselected.includes(i))
+            this.selected.push(i)
+        }
+      }
+      console.log(this.selected)
+    }
   },
   data: function () {
     return {
+      view: false,
       name: '',
       selected: [],
       data: data,
@@ -15,15 +35,14 @@ new Vue({
     click: function (idx) {
       console.log(idx)
       var square = document.getElementById(`square-${idx}`)
-      if (square.classList.contains('selected')) {
-        square.classList.remove('selected')
+      if (this.selected.includes(idx)) {
         this.selected.splice(this.selected.indexOf(idx), 1)
       } else {
-        square.classList.add('selected')
         this.selected.push(idx)
         this.selected.sort((a, b) => a - b)
       }
       console.log(this.selected)
+      this.save()
     },
     pack: function (array) {
       var packed = []
@@ -34,8 +53,28 @@ new Vue({
       }
       return packed.join('')
     },
-    load: function (packed) {
-      return packed.match(/\d\d/gi).map(t => parseInt(t))
+    unpack: function (packed) {
+      return packed.match(/\d\d/gi).map(t => parseInt(t, 16))
+    },
+    save: function () {
+      if (this.selected.length > 12) {
+        unselected = []
+        for (let i = 0; i < 25; i++) {
+          if (!this.selected.includes(i))
+            unselected.push(i)
+        }
+        console.log('?f=' + this.pack(unselected) + '&name=' + this.name) 
+      } else {
+        console.log('?t=' + this.pack(this.selected) + '&name=' + this.name)
+      }
+    },
+    getSelectedStyle: function (idx) {
+      if (this.selected.includes(idx)) {
+        return {
+          color: '#FFE792',
+          backgroundColor: '#75715E'
+        }
+      }
     }
   }
 })
